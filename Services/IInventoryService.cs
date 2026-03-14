bcc4fb78e4af61d91856ca9dd63e963c7a5edef8
unknown <fegalterio04>
2026-03-13 21:15:26 -0300

    feat: Adicionando Service de inventario

---
+++ Services/IInventoryService.cs
@@ -0,0 +1,13 @@
+﻿using ProjetoFênix.Models;
+
+namespace ProjetoFenix.Services
+{
+    public interface IInventoryService
+    {
+        List<Item> GetAll();
+        Item? GetById(int id);
+        void Add(Item item);
+        void Update(int id, Item item);
+        void Delete(int id);
+    }
+}
---
+++ Services/InventoryService.cs
@@ -0,0 +1,44 @@
+﻿using ProjetoFênix.Models;
+using System.Xml.Linq;
+
+namespace ProjetoFenix.Services
+{
+    public class InventoryService : IInventoryService
+    {
+        private readonly List<Item> _items = new List<Item>();
+        private int _nextId = 1;
+
+        public List<Item> GetAll()
+        {
+            return _items;
+        }
+
+        public Item? GetById(int id)
+        {
+            return _items.FirstOrDefault(i => i.Id == id);
+        }
+
+        public void Add(Item item)
+        {
+            item.Id = _nextId++;
+            _items.Add(item);
+        }
+
+        public void Update(int id, Item itemAtualizado)
+        {
+            var item = GetById(id);
+            if (item == null) return;
+
+            item.Nome = itemAtualizado.Nome;
+            item.Quantidade = itemAtualizado.Quantidade;
+            item.Categoria = itemAtualizado.Categoria;
+        }
+
+        public void Delete(int id)
+        {
+            var item = GetById(id);
+            if (item != null)
+                _items.Remove(item);
+        }
+    }
+}
