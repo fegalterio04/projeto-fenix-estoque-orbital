cf202dd4e1af0a400f03d1c1053f8cafc8d91fbe
unknown <fegalterio04>
2026-03-13 21:08:43 -0300

    feat: criando classe item

---
+++ Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoFênix.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int Quantidade { get; set; }
+        public string Categoria { get; set; } = string.Empty;
+    }
+}
