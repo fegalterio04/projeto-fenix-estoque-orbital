d7198e4e2a5e66b23360201da685957d6b8383b2
unknown <fegalterio04>
2026-03-15 20:59:02 -0300

    feat: Cria ItensController com endpoints GET, POST, PUT e DELETE

---
+++ Controllers/ItensController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoFênix.Models;
+using ProjetoFênix.Services;
+
+namespace ProjetoFênix.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly IInventoryService _service;
+
+        public ItemsController(IInventoryService service)
+        {
+            _service = service;
+        }
+
+        // GET /items
+        public IActionResult Index()
+        {
+            var items = _service.GetAll();
+            return View(items);
+        }
+
+        // POST /items/create
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _service.Add(item);
+            return RedirectToAction("Index");
+        }
+
+        // POST /items/edit/{id}
+        [HttpPost]
+        public IActionResult Edit(int id, Item item)
+        {
+            _service.Update(id, item);
+            return RedirectToAction("Index");
+        }
+
+        // POST /items/delete/{id}
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
