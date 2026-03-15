6fe5eca6a2abd279f56596a76ed64171c7efd09d
unknown <fegalterio04>
2026-03-15 20:50:37 -0300

    Chore: Atualizando Git Ignore e acertando Services com DI

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using ProjetoFênix.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IInventoryService, InventoryService>();
 
 var app = builder.Build();
 
@@ -9,15 +12,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
